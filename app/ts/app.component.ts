59d30ca46ae91de91b99186267e3be753a064874
iodum <violetiodum@gmail.com>
2017-06-21 00:18:46 +0300

    Add video playlist

--- app/ts/app.component.ts
+++ app/ts/app.component.ts
@@ -1,8 +1,22 @@
 import {Component} from 'angular2/core';
+import {Config} from './config.service';
+import {Video} from './video';
+import {PlaylistComponent} from './playlist.component'
 
 @Component({
     selector: 'my-app',
-    template: '<h1>Angular 2 WoW!</h1>'
+    templateUrl: 'app/ts/app.component.html',
+	directives: [PlaylistComponent]
 })
 
-export class AppComponent {}
+export class AppComponent {
+	mainHeading = Config.MAIN_HEADING;
+	videos:Array<Video>;
+	
+	constructor(){
+		this.videos = [
+			new Video(1, "Getting Started", "hXfigUyeHaY", "What is Angular 2?﻿"),
+			new Video(2, "Overview and Core Concepts", "vc9n0j6-D2U", "Bla-bla-bla﻿")
+		]
+	}
+}
